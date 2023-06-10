7fcc31c950a63b7bb816f01c47779cbdfbf9d319
Anukul <anukulpk2@gmail.com>
2023-06-10 13:44:16 +0530

    player death added

---
+++ Assets/Scripts/PlayerDeathScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ///if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            //Level is over
+            Debug.Log("Oops, you are dead");
+        }
+    }
+}
